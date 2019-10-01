363501cee55eaecd4e06090ef3b77845933140cb
Daryk Kohler <Ilithor@users.noreply.github.com>
2019-10-01 13:29:15 -0700

    Delete environment.js

--- server/environment/environment.js
+++
@@ -1,9 +0,0 @@
-const env = {
-  dbName: 'react-social-app',
-  key: 'sb5uhWYIggfDk5t1VgPty1fa4cAqz6aI8iVN9ctph2Z7ka3LJxHJHFmI6AltFuXxsxNcmlkTQaWk4HehqD2MMg==',
-  cosmosPort: 10255,
-  localUsername: 'Ilithor',
-  localPassword: 'DarthZorn555'
-};
-
-module.exports = env;
