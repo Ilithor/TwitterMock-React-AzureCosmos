20f007bd7a2d42cb2728699a01a62ec49fa1521f
Ilithor <daryk.kohler@gmail.com>
2019-10-30 16:37:59 -0700

    Posts made by all users now display on the homepage

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,40 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import Grid from '@material-ui/core/Grid';
+
+import Post from '../components/Post';
 
 export class home extends Component {
+  state = {
+    postList: null
+  };
+
+  componentDidMount() {
+    axios
+      .get('/api/post')
+      .then(res => {
+        console.log(res.data);
+        this.setState({
+          postList: res.data
+        });
+      })
+      .catch(err => console.log(err));
+  }
   render() {
+    let recentPostMarkup = this.state.postList ? (
+      this.state.postList.map(post => <Post post={post} />)
+    ) : (
+      <p>Loading...</p>
+    );
     return (
-      <div>
-        <h1>Home Page</h1>
-      </div>
+      <Grid container spacing={16}>
+        <Grid item sm={8} xs={12}>
+          {recentPostMarkup}
+        </Grid>
+        <Grid item sm={4} xs={12}>
+          <p>Profile...</p>
+        </Grid>
+      </Grid>
     );
   }
 }
