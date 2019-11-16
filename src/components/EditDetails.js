ec1ec7c5d415ff61b3fcaa16ebbdda969b5ab5ef
Ilithor <daryk.kohler@gmail.com>
2019-11-16 15:39:19 -0800

    Added Custom Buttons
    Replaced tooltip button for editing profile info with the custom button component.

--- src/components/EditDetails.js
+++ src/components/EditDetails.js
@@ -2,14 +2,13 @@ import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import withStyles from '@material-ui/core/styles/withStyles';
 import style from '../style/style';
+import CustomButton from '../util/CustomButton';
 
 // Redux
 import { connect } from 'react-redux';
 import { editUserDetailAction } from '../redux/actions/userActions';
 
 // MUI
-import Tooltip from '@material-ui/core/Tooltip';
-import IconButton from '@material-ui/core/IconButton';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
@@ -70,11 +69,13 @@ class EditDetails extends Component {
     const { classes } = this.props;
     return (
       <Fragment>
-        <Tooltip title='Edit details' placement='top'>
-          <IconButton onClick={this.handleOpen} className={classes.buttonEdit}>
-            <EditIcon color='primary' />
-          </IconButton>
-        </Tooltip>
+        <CustomButton
+          tip='Edit Details'
+          onClick={this.handleOpen}
+          btnClassName={classes.buttonEdit}
+        >
+          <EditIcon color='primary' />
+        </CustomButton>
         <Dialog
           open={this.state.open}
           onClose={this.handleClose}
