ab8992a761e190706061f3ee8474f56eaf821c04
Jefferson Eagley <jefferson.eagley@gmail.com>
2019-12-03 19:48:32 -0800

    clean up type exports

--- server/util/mongo.js
+++ server/util/mongo.js
@@ -3,6 +3,7 @@ import env from '../environment/environment';
 
 mongoose.Promise = global.Promise;
 
+// eslint-disable-next-line no-unused-vars
 const mongoUri = `mongodb://${env.dbName}:${env.key}@${env.dbName}.documents.azure.com:${env.cosmosPort}/${env.dbName}?ssl=true&replicaSet=globaldb`;
 const localMongoUri = `mongodb+srv://${env.localUsername}:${env.localPassword}@gonkzonk-ytla5.azure.mongodb.net/test?retryWrites=true&w=majority`;
 
@@ -10,6 +11,6 @@ export default () => {
   return mongoose.connect(localMongoUri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true
+    useCreateIndex: true,
   });
 };
