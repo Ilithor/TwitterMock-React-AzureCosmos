275fc57b2f820d9dd442771de8a29487d8b524dd
Jefferson Eagley <jefferson.eagley@gmail.com>
2019-12-06 14:30:25 -0800

    the simplest possible component

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -12,37 +12,46 @@ import style from '../../style/style';
 // Icons
 import AppIcon from '../../images/icon.png';
 
-const Login = props => {
-  const {
-    classes,
-    error,
-    email,
-    password,
-    handleSubmit,
-    handleChange,
-    isLoading,
-  } = props;
-  return (
-    <Grid container className={classes.form}>
-      <Grid item sm />
-      <Grid item sm>
-        <img src={AppIcon} alt='eye' className={classes.image} />
-        <Typography variant='h2' className={classes.pageTitle}>
-          Login
-        </Typography>
-        <LoginForm
-          classes={classes}
-          error={error}
-          email={email}
-          password={password}
-          handleSubmit={handleSubmit}
-          handleChange={handleChange}
-          isLoading={isLoading}
-        />
-      </Grid>
-      <Grid item sm />
+/** Control that allows the user to log in
+ *
+ * @type {React.FunctionComponent}
+ * @param {object} props
+ * @param {any} props.error
+ * @param {object} props.classes
+ * @param {string} props.email
+ * @param {string} props.password
+ * @param {React.ChangeEventHandler} props.handleSubmit
+ * @param {React.ChangeEventHandler} props.handleChange
+ * @param {boolean} props.isLoading
+ */
+const Login = ({
+  classes,
+  error,
+  email,
+  password,
+  handleSubmit,
+  handleChange,
+  isLoading,
+}) => (
+  <Grid container className={classes.form}>
+    <Grid item sm />
+    <Grid item sm>
+      <img src={AppIcon} alt='eye' className={classes.image} />
+      <Typography variant='h2' className={classes.pageTitle}>
+        Login
+      </Typography>
+      <LoginForm
+        classes={classes}
+        error={error}
+        email={email}
+        password={password}
+        handleSubmit={handleSubmit}
+        handleChange={handleChange}
+        isLoading={isLoading}
+      />
     </Grid>
-  );
-};
+    <Grid item sm />
+  </Grid>
+);
 
 export default withStyles(style)(Login);
