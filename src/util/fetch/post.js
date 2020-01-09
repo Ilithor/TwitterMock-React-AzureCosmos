a314fd6a3d632ec755a1350a42f0b8b20ba79a85
Ilithor <daryk.kohler@gmail.com>
2020-01-08 22:19:00 -0800

    Converted to es6

--- src/util/fetch/post.js
+++ src/util/fetch/post.js
@@ -13,7 +13,7 @@ export const fetchPostList = () => get(endpoints.post);
 /** Fetch a specific post
  * @returns {Promise<import("axios").AxiosResponse>}
  */
-export const fetchPost = postId => get(endpoints.post + postId);
+export const fetchPost = postId => get(`${endpoints.post}${postId}`);
 
 /** Creates a new post
  * @returns {Promise<import("axios").AxiosResponse>}
@@ -23,20 +23,20 @@ export const createPost = newPost => post(endpoints.post, newPost);
 /** Likes a post
  * @returns {Promise<import("axios").AxiosResponse>}
  */
-export const likePost = postId => get(endpoints.post + postId + '/like');
+export const likePost = postId => get(`${endpoints.post}${postId}/like`);
 
 /** Unlikes a post
  * @returns {Promise<import("axios").AxiosResponse>}
  */
-export const unlikePost = postId => get(endpoints.post + postId + '/unlike');
+export const unlikePost = postId => get(`${endpoints.post}${postId}/unlike`);
 
 /** Comment on post
  * @returns {Promise<import("axios").AxiosResponse>}
  */
 export const commentPost = (postId, commentData) =>
-  post(endpoints.post + postId + '/comment', commentData);
+  post(`${endpoints.post}${postId}/comment`, commentData);
 
 /** Deletes a post
  * @returns {Promise<import("axios").AxiosResponse>}
  */
-export const deletePost = postId => remove(endpoints.post + postId);
+export const deletePost = postId => remove(`${endpoints.post}${postId}`);
