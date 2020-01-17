5ab0ddb7af273b48fe4d0316e1f2c02aa6309c6b
Ilithor <daryk.kohler@gmail.com>
2020-01-17 00:27:11 -0800

    Refactored to reflect recent changes

--- src/components/profile/static/ProfileDetails.js
+++ src/components/profile/static/ProfileDetails.js
@@ -1,69 +1,31 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import dayjs from 'dayjs';
 import { Link } from 'react-router-dom';
 
+// Components
+import Location from './profileBio/Location';
+import Website from './profileBio/Website';
+import AboutMe from './profileBio/AboutMe';
+
 // MUI
 import MuiLink from '@material-ui/core/Link';
-import Typography from '@material-ui/core/Typography';
 
 // Icons
 import CalendarToday from '@material-ui/icons/CalendarToday';
-import LinkIcon from '@material-ui/icons/Link';
-import LocationOn from '@material-ui/icons/LocationOn';
 
-const ProfileDetails = ({ handle, bio, createdAt }) => {
-  const createBio = () => {
-    if (!!bio.aboutMe) {
-      return <Typography variant='body2'>{bio.aboutMe}</Typography>;
-    }
-    return <div />;
-  };
-
-  const createLocation = () => {
-    if (!!bio.location) {
-      return (
-        <Fragment>
-          <LocationOn color='primary' />
-          <span>{bio.location}</span>
-          <hr />
-        </Fragment>
-      );
-    }
-    return <div />;
-  };
-
-  const createWebsite = () => {
-    if (bio.website) {
-      const link = (
-        <a href={bio.website} target='_blank' rel='noopener noreferrer'>
-          {' '}
-          {bio.website}
-        </a>
-      );
-      return (
-        <Fragment>
-          <LinkIcon color='primary' />
-          {link}
-          <hr />
-        </Fragment>
-      );
-    }
-    return <div />;
-  };
-  return (
-    <div className='profile-details'>
-      <MuiLink component={Link} to={`/${handle}`} color='primary' variant='h5'>
-        @{handle}
-      </MuiLink>
-      <hr />
-      {createBio()}
-      <hr />
-      {createLocation()}
-      {createWebsite()}
-      <CalendarToday color='primary' />{' '}
-      <span>Joined {dayjs(createdAt).format('MMM YYYY')}</span>
-    </div>
-  );
-};
+const ProfileDetails = ({ handle, bio, createdAt }) => (
+  <div className='profile-details'>
+    <MuiLink component={Link} to={`/${handle}`} color='primary' variant='h5'>
+      @{handle}
+    </MuiLink>
+    <hr />
+    <AboutMe aboutMe={bio.aboutMe} />
+    <hr />
+    <Location location={bio.location} />
+    <Website website={bio.website} />
+    <CalendarToday color='primary' />{' '}
+    <span>Joined {dayjs(createdAt).format('MMM YYYY')}</span>
+  </div>
+);
 
 export default ProfileDetails;
