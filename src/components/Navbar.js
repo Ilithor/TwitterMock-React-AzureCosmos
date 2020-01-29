a08774544857d1e6417b47921ac1bd7fc43a5dc9
Ilithor <daryk.kohler@gmail.com>
2020-01-28 19:36:55 -0800

    Imported data where applicable

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,11 +17,10 @@ import {
 } from './profile/userContext';
 
 /** Shows the log in or log out button
- * @param {object} props
- * @param {boolean} props.isAuthenticated
- * @param {any} props.logoutUser
  */
-const ButtonLogInOut = ({ isAuthenticated, logoutUser }) => {
+const ButtonLogInOut = () => {
+  const { isAuthenticated } = useUserAuthenticationData();
+  const { logoutUser } = useUserLogout();
   if (isAuthenticated) {
     return (
       <CustomButton tip='Logout' onClick={logoutUser}>
@@ -37,10 +36,9 @@ const ButtonLogInOut = ({ isAuthenticated, logoutUser }) => {
 };
 
 /** Displays either signup button or empty div
- * @param {object} props
- * @param {boolean} props.isAuthenticated
  */
-const ButtonRegister = ({ isAuthenticated }) => {
+const ButtonRegister = () => {
+  const { isAuthenticated } = useUserAuthenticationData();
   if (isAuthenticated) {
     return <div></div>;
   }
@@ -52,10 +50,9 @@ const ButtonRegister = ({ isAuthenticated }) => {
 };
 
 /** Displays either notification button or empty div
- * @param {object} props
- * @param {boolean} props.isAuthenticated
  */
-const ButtonNotification = ({ isAuthenticated }) => {
+const ButtonNotification = () => {
+  const { isAuthenticated } = useUserAuthenticationData();
   if (isAuthenticated) {
     return (
       <CustomButton tip='Notifications' component={Link} to='/notification'>
@@ -68,22 +65,15 @@ const ButtonNotification = ({ isAuthenticated }) => {
 
 /** View component for navbar
  */
-export const Navbar = () => {
-  const { isAuthenticated } = useUserAuthenticationData();
-  const { logoutUser } = useUserLogout();
-  return (
-    <AppBar>
-      <Toolbar className='nav-container'>
-        <CustomButton tip='Home' component={Link} to='/'>
-          <Icon.Home />
-        </CustomButton>
-        <ButtonNotification isAuthenticated={isAuthenticated} />
-        <ButtonLogInOut
-          isAuthenticated={isAuthenticated}
-          logoutUser={logoutUser}
-        />
-        <ButtonRegister isAuthenticated={isAuthenticated} />
-      </Toolbar>
-    </AppBar>
-  );
-};
+export const Navbar = () => (
+  <AppBar>
+    <Toolbar className='nav-container'>
+      <CustomButton tip='Home' component={Link} to='/'>
+        <Icon.Home />
+      </CustomButton>
+      <ButtonNotification />
+      <ButtonLogInOut />
+      <ButtonRegister />
+    </Toolbar>
+  </AppBar>
+);
