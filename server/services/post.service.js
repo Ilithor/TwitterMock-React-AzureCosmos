d946033c5a18becbb69f7911a77e86592cd4f320
Jefferson Eagley <jefferson.eagley@gmail.com>
2020-02-03 14:46:24 -0800

    Refactor in progress

--- server/services/post.service.js
+++ server/services/post.service.js
@@ -7,48 +7,39 @@ mongoConnection();
 /** Retrieves all posts in desc order
  * @returns {Promise<post[Post[]]>}
  */
-export const getList = () =>
-  new Promise(resolve => {
-    const postList = Post.find({})
-      .sort({ createdAt: -1 })
-      .read(mongo.ReadPreference.NEAREST)
-      .limit(100);
-    resolve(postList);
-  });
+export const getList = async () => {
+  return await Post.find({})
+    .sort({ createdAt: -1 })
+    .read(mongo.ReadPreference.NEAREST)
+    .limit(100);
+};
 
 /** Creates and saves new post
  * @param {Object} postParam
  * @param {User} user
  * @returns {Promise<Post>}
  */
-export const create = (postParam, user) =>
-  new Promise((resolve, reject) => {
-    // Validation
-    if (postParam.body.trim() === '') {
-      reject({ body: 'Body must not be empty' });
-    }
-    // Create new post
-    postParam.userHandle = user.userHandle;
-    const newPost = new Post(postParam);
-    newPost.likeCount = 0;
-    newPost.commentCount = 0;
+export const create = async (postParam, user) => {
+  // Validation
+  if (postParam.body.trim() === '') {
+    return Promise.reject({ body: 'Body must not be empty' });
+  }
+  // Create new post
+  postParam.userHandle = { ...user.userHandle };
+  const newPost = new Post(postParam);
+  newPost.likeCount = 0;
+  newPost.commentCount = 0;
 
-    // Save post
-    newPost.save();
-    resolve(newPost);
-  });
+  // Save post
+  await newPost.save();
+  return Promise.resolve(newPost);
+};
 
 /** Deletes post
  * @param {String} _id
  */
-export const findAndDeletePost = _id =>
-  new Promise((resolve, reject) => {
-    try {
-      const post = Post.deleteOne({
-        _id,
-      });
-      resolve(post);
-    } catch (err) {
-      reject(err);
-    }
+export const findAndDeletePost = async _id => {
+  return await Post.deleteOne({
+    _id,
   });
+};
