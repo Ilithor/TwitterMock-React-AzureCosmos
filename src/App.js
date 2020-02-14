77c2107dec8fb3a71a01eb0f34622910466a18d9
Ilithor <daryk.kohler@gmail.com>
2020-02-13 17:04:27 -0800

    Implemented react-helmet to display description

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import axios from 'axios';
+import { Helmet } from 'react-helmet';
 
 // Pages
 import { HomePage } from './pages/home';
@@ -23,25 +24,39 @@ axios.defaults.baseURL = '/';
  *
  * @type {React.FunctionComponent}
  */
-export const App = () => (
-  <Router>
-    <ContextProvider>
-      <Navbar />
-      <div className='container'>
-        <Switch>
-          <Route exact path='/' component={HomePage} />
-          <Route path='/login' component={LoginPage} />
-          <Route path='/signup' component={RegisterPage} />
-          <Route path='/settings' component={SettingPage} />
-          <Route path='/notification' component={NotificationPage} />
-          <Route exact path='/u/:userHandle' component={UserPage} />
-          <Route
-            exact
-            path='/u/:userHandle/post/:postId'
-            component={UserPage}
-          />
-        </Switch>
-      </div>
-    </ContextProvider>
-  </Router>
-);
+export const App = () => {
+  const AppContent = () => (
+    <Router>
+      <ContextProvider>
+        <Navbar />
+        <div className='container'>
+          <Switch>
+            <Route exact path='/' component={HomePage} />
+            <Route path='/login' component={LoginPage} />
+            <Route path='/signup' component={RegisterPage} />
+            <Route path='/settings' component={SettingPage} />
+            <Route path='/notification' component={NotificationPage} />
+            <Route exact path='/u/:userHandle' component={UserPage} />
+            <Route
+              exact
+              path='/u/:userHandle/post/:postId'
+              component={UserPage}
+            />
+          </Switch>
+        </div>
+      </ContextProvider>
+    </Router>
+  );
+  return (
+    <div>
+      <Helmet>
+        <title>SocMon</title>
+        <meta
+          name='description'
+          content='SocMon is a place to meet and communicate with friends and family. Create posts, then comment and like on what others have posted.'
+        />
+      </Helmet>
+      <AppContent />
+    </div>
+  );
+};
