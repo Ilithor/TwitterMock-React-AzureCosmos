1efbb6c92b22e46641f972f953fc41477dbe989b
Ilithor <daryk.kohler@gmail.com>
2020-02-18 15:27:02 -0800

    Converted footer over to just a div

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,20 +4,13 @@ import { Link } from 'react-router-dom';
 // Components
 import { CustomButton } from '../util/CustomButton';
 
-// MUI
-import { AppBar, Toolbar } from '@material-ui/core';
-
 // Icons
 import * as Icon from '@material-ui/icons';
 
 export const Footer = () => (
   <div className='footer-container'>
-    <AppBar>
-      <Toolbar className='footer-buttons'>
-        <CustomButton tip='About Us' component={Link} to='/about'>
-          <Icon.Info />
-        </CustomButton>
-      </Toolbar>
-    </AppBar>
+    <CustomButton tip='About Us' component={Link} to='/about'>
+      <Icon.Info />
+    </CustomButton>
   </div>
 );
